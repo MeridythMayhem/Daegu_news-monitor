277f860021426d8136c412e2ce7340c2671d43c1
MeridythMayhem <zichun24@gmail.com>
2026-04-07 21:05:08 +0900

    cloud_bot.py 업데이트

--- cloud_bot.py
+++ cloud_bot.py
@@ -12,7 +12,7 @@ from difflib import SequenceMatcher
 # =========================================================
 # [1] 환경변수 및 설정
 # =========================================================
-TEST_MODE = True  
+TEST_MODE = False  
 
 NAVER_CLIENT_ID = os.environ.get("NAVER_ID")
 NAVER_CLIENT_SECRET = os.environ.get("NAVER_SECRET")
